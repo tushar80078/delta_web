ef227bde89442081138dea428ff0125f589a118c
Tushar Bhosale <tusharbhosale2281@gmail.com>
2025-02-17 12:08:25 +0530

    Add to cart

--- src/pages/public/CourseInfo/index.jsx
+++ src/pages/public/CourseInfo/index.jsx
@@ -6,7 +6,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import { FaStar } from "react-icons/fa";
 import { MdLanguage } from "react-icons/md";
 import simple from "../../../assets/images/loginImage.png";
-import AddToCart from "./components/AddToCart";
+import AddToCart from "./components/AddToCart.jsx";
 import { courseDetailsTab } from "./constant";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
