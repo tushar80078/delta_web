ece3a1613de23049e5fc19451c9e915b80ca7e5b
Tushar Bhosale <tusharbhosale2281@gmail.com>
2025-02-17 14:02:53 +0530

    check

--- src/pages/public/CourseInfo/index.jsx
+++ src/pages/public/CourseInfo/index.jsx
@@ -10,7 +10,7 @@ import simple from "../../../assets/images/loginImage.png";
 import { courseDetailsTab } from "./constant";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import Cart from "./components/Cart";
+import Cart from "@/pages/public/CourseInfo/components/Cart";
 
 const CourseDetails = () => {
   const { id } = useParams();
