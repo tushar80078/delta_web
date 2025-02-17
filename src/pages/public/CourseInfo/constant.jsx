43be8521aff8a454ad5f253b4adcd2a2ac7dabf4
Tushar Bhosale <tusharbhosale2281@gmail.com>
2025-02-17 12:23:06 +0530

    check

--- src/pages/public/CourseInfo/constant.jsx
+++ src/pages/public/CourseInfo/constant.jsx
@@ -1,10 +1,10 @@
 import facebook from "../../../assets/images/Facebook_Logo.png";
 import Google from "../../../assets/images/google.png";
 import Microsoft from "../../../assets/images/google.png";
-import Description from "./components/Description";
-import Instructor from "./components/Instructor";
-import Reviews from "./components/Reviews";
-import Syllabus from "./components/Syllabus";
+import Description from "@/pages/public/CourseInfo/components/Description";
+import Instructor from "@/pages/public/CourseInfo/components/Instructor";
+import Reviews from "@/pages/public/CourseInfo/components/Reviews";
+import Syllabus from "@/pages/public/CourseInfo/components/Syllabus";
 
 export const signInMethodsDetails = [
   {
